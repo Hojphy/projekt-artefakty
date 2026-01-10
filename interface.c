051e00c34d681149f8d3f791a7f73b2646883e80
Hojphy <kareckirobert@gmail.com>
2026-01-10 23:06:53 +0100

    interface: wyglad menu i jego czesciowa funkcjonalnosc

--- interface.c
+++ interface.c
@@ -2,30 +2,83 @@
 
 void show_menu()
 {
-    printf("+---Galaktyczne Archiwum---+\n");
-    printf("1. Dodaj artefakt\n");
-    printf("2. Wypisz zawartosc archiwum\n");
-    printf("3. Zapisz i wyjdz\n");
-    printf("+--------------------------+\n");
+    printf("\n+========== SYSTEM ZARZADZANIA ARTEFAKTAMI ==========+\n");
+    printf("| 1. %-47s |\n", "Dodaj nowy artefakt");
+    printf("| 2. %-47s |\n", "Edytuj istniejacy artefakt");
+    printf("| 3. %-47s |\n", "Usun artefakt z bazy");
+    printf("| %50s |\n", "--------------------------------------------------");
+    printf("| 4. %-47s |\n", "Wyswietl wszystkie artefakty (Tabela)");
+    printf("| 5. %-47s |\n", "Wyszukaj po nazwie (szukanie tekstu)");
+    printf("| 6. %-47s |\n", "Filtruj po zagrozeniu (szukanie liczby)");
+    printf("| %50s |\n", "--------------------------------------------------");
+    printf("| 7. %-47s |\n", "Posortuj baze danych");
+    printf("| 8. %-47s |\n", "Przelacz kierunek sortowania (Rosnaco/Malejaco)"); 
+    printf("| %50s |\n", "--------------------------------------------------");
+    printf("| 9. %-47s |\n", "Zapisz zmiany do pliku (bez wychodzenia)");
+    printf("| 0. %-47s |\n", "Zapisz i wyjdz");
+    printf("+====================================================+\n");
+    printf("Twoj wybor: ");
     
 }
 
 void run_loop(Archive* archive, const char* db_filename)
 {
     int running = 1;
+    int current_mode = NAME_SORT;
+    int current_direction = ASCENDING_SORT;
     while (running) 
     {
-        // clear_console();
+        clear_console();
         show_menu();
         int choice = get_int_input();
         switch (choice) 
         {
-            case 1:
+            case MENU_ADD:
+                handle_add_artifact(archive);
+
+                wait_for_enter();
                 break;
-            case 2:
+            case MENU_EDIT:
+                break;
+            case MENU_REMOVE:
+                break;
+            case MENU_PRINT_ALL:
                 print_archive(archive);
+                wait_for_enter();
                 break;
-            case 3:
+            case MENU_SEARCH_NAME:
+                break;
+            case MENU_SEARCH_THREAT:
+                break;
+            case MENU_SORT:
+                current_mode = get_sort_criteria_from_user();
+                sort_archive(archive, current_mode | current_direction);
+                printf("Posortowano!\n");
+
+                wait_for_enter();
+                break;
+            case MENU_TOGGLE_DIR:
+                if (current_direction == ASCENDING_SORT) 
+                {
+                    current_direction = DESCENDING_SORT;
+                    printf("Zmieniono na: MALEJACO\n");
+                }
+                else 
+                {
+                    current_direction = ASCENDING_SORT;
+                    printf("Zmieniono na: ROSNACO\n");
+                }
+                sort_archive(archive, current_mode | current_direction);
+
+                wait_for_enter();
+                break;
+            case MENU_SAVE:
+                save_to_file(archive, db_filename);
+                printf("Zapisano!\n");
+                
+                wait_for_enter();
+                break;
+            case MENU_EXIT:
                 save_to_file(archive, db_filename);
                 running = 0;
                 break;
@@ -39,6 +92,17 @@ static void clear_input_buffer()
 {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
+}
+
+static void clear_console()
+{
+
+}
+
+static void wait_for_enter()
+{
+    printf("\n[Nacisnij ENTER, aby kontynuowac...]");
+    clear_input_buffer();
 }
 
 static void get_string_input(char *buffer, int size) 
@@ -67,4 +131,39 @@ static int get_int_input()
     }
     clear_input_buffer();
     return value;
+}
+
+static void handle_add_artifact(Archive* arch)
+{
+    
+}
+
+static int get_sort_criteria_from_user() 
+{
+    printf("\n--- WYBIERZ KRYTERIUM ---\n");
+    printf("1. Nazwa\n");
+    printf("2. Pochodzenie\n");
+    printf("3. Poziom zagrozenia\n");
+    printf("4. Rok odkrycia\n");
+    printf("5. Status\n");
+    printf("Wybor: ");
+    
+    int c = get_int_input();
+    
+    switch(c) 
+    {
+        case 1: 
+            return NAME_SORT;
+        case 2: 
+            return ORIGIN_SORT;
+        case 3: 
+            return THREAT_SORT;
+        case 4: 
+            return YEAR_SORT;
+        case 5: 
+            return STATUS_SORT;
+        default: 
+            printf("Nieprawidlowy wybor. Domyslnie: Nazwa.\n");
+            return NAME_SORT;
+    }
 }
--- interface.h
+++ interface.h
@@ -5,15 +5,33 @@
 #include <stdlib.h>
 #include "artifacts.h"
 
+enum MenuOption 
+{
+    MENU_EXIT = 0,
+    MENU_ADD = 1,
+    MENU_EDIT = 2,
+    MENU_REMOVE = 3,
+    MENU_PRINT_ALL = 4,
+    MENU_SEARCH_NAME = 5,
+    MENU_SEARCH_THREAT = 6,
+    MENU_SORT = 7,
+    MENU_TOGGLE_DIR = 8,
+    MENU_SAVE = 9
+};
+
 void show_menu();
 void run_loop(Archive* archive, const char* db_filename);
-void clear_console();
 
 void print_artifacts_by_fragment(const Archive *arch, const char *fragment);
 void print_artifacts_by_threat(const Archive *arch, const int level);
 
 static void clear_input_buffer();
+static void clear_console();
 static void get_string_input(char *buffer, int size);
 static int get_int_input();
+static void wait_for_enter();
+static int get_sort_criteria_from_user();
+
+static void handle_add_artifact(Archive* arch);
 
 #endif
